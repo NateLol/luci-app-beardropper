db95f25aeb48b45c8fcb7f2c09c1ddde80a655fe
natelol <ding184753928@126.com>
2020-05-23 08:38:38 +0800

    Support for Logs

--- luasrc/controller/beardropper.lua
+++ luasrc/controller/beardropper.lua
@@ -6,7 +6,8 @@ function index()
     end
     entry({"admin", "services", "beardropper"}, alias("admin", "services", "beardropper", "setting"),_("BearDropper"), 30).dependent = true
     entry({"admin", "services", "beardropper", "status"}, call("act_status")).leaf= true
-    entry({"admin", "services", "beardropper", "setting"}, cbi("beardropper/setting"), _(""), 1)
+    entry({"admin", "services", "beardropper", "setting"}, cbi("beardropper/setting"), _("Setting"), 1)
+    entry({"admin", "services", "beardropper", "log"}, cbi("beardropper/log"),_("Log"),2)
     --entry:
 end
 
