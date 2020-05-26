dc7892e8146089a77d7c314250d16ce13fc368f2
natelol <ding184753928@126.com>
2020-05-26 22:17:14 +0800

    save log to /tmp/beardropper.log

--- luasrc/model/cbi/beardropper/log.lua
+++ luasrc/model/cbi/beardropper/log.lua
@@ -3,9 +3,9 @@ f.reset = false
 f.submit = false
 t = f:field(TextValue, "conf")
 t.rmempty = true
-t.rows = 20
+t.rows = 40
 function t.cfgvalue()
-local logs = luci.util.execi("logread | grep authpriv | grep beardropper")
+local logs = luci.util.execi("if [ -f /tmp/beardropper.log ];then cat /tmp/beardropper.log; else touch /tmp/beardropper.log; fi")
 local s = ""
 for line in logs do
 s = line .. "\n" .. s
